f5c45370ea3e0365632af5f205d7a176613a1b0e
jonas souza <jonassouza305@yahoo.com.br>
2025-04-16 23:10:48 -0300

    fix:fixes bug in dependencies

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,19 +1,15 @@
-import { BrowserRouter,Route,Routes } from "react-router-dom"
-import { PokemonsList } from "../components/pokemons"
-import { PokemonDetail } from "../components/pokemon"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { PokemonsList } from "../components/pokemons";
+import { PokemonDetail } from "../components/pokemon";
 
-
-
-
-
-const AppRoutes = ()=>{
-    return (
-    <BrowserRouter>
-    <Routes>
-        <Route path='/'  element={<PokemonsList/>} />
-        <Route path='/pokemon/:name' element={<PokemonDetail/>} />
-    </Routes>
+const AppRoutes = () => {
+  return (
+    <BrowserRouter basename="/react-avancado-pokemons">
+      <Routes>
+        <Route path="/" element={<PokemonsList />} />
+        <Route path="/pokemon/:name" element={<PokemonDetail />} />
+      </Routes>
     </BrowserRouter>
-    )
-}
-export {AppRoutes}
+  );
+};
+export { AppRoutes };
